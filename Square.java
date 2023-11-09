ec805de00c6fd7c12963cd668104d326abcd4953
dzulfikarmagang123 <fikaralghifari0504@gmail.com>
2023-11-09 08:05:52 +0700

    Percobaan 1 & 2

---
+++ Square.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N : ");
+        int N = sc.nextInt();
+        for(int i=1; i<=N; i++){
+            for(int j=0; j<=N; j++){
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
